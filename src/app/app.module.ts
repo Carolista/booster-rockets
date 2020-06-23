201aaf30647c02906dad1e13346e7b7325d3760c
Carolista <cjones.renaissancewoman@gmail.com>
2020-06-23 05:27:53 -0500

    set up project with misc components and classes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ScorecardComponent } from './scorecard/scorecard.component';
+import { FlashcardComponent } from './flashcard/flashcard.component';
+import { OptionsComponent } from './options/options.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ScorecardComponent,
+    FlashcardComponent,
+    OptionsComponent
   ],
   imports: [
     BrowserModule,
