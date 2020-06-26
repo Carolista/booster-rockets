2854074b29b695bf2ef1190629223b8ced55e85c
Carolista <cjones.renaissancewoman@gmail.com>
2020-06-26 00:34:26 -0500

    built Spring/Gradle application and set up directories for controllers, models, and security

--- back-end/src/main/java/com/kaleidoscope314/boosterrockets/BoosterRocketsApplication.java
+++ back-end/src/main/java/com/kaleidoscope314/boosterrockets/BoosterRocketsApplication.java
@@ -1,4 +1,4 @@
-package com.kaleidoscope.boosterrockets;
+package com.kaleidoscope314.boosterrockets;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- back-end/src/test/java/com/kaleidoscope314/boosterrockets/BoosterRocketsApplicationTests.java
+++ back-end/src/test/java/com/kaleidoscope314/boosterrockets/BoosterRocketsApplicationTests.java
@@ -1,4 +1,4 @@
-package com.kaleidoscope.boosterrockets;
+package com.kaleidoscope314.boosterrockets;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
