b60bbfeb641f4bd7a7535b6b16aebd87d328cc5d
Carolista <cjones.renaissancewoman@gmail.com>
2020-06-26 13:32:41 -0500

    got controllers and most classes needed for authentication in place

---
+++ back-end/src/main/java/com/kaleidoscope314/boosterrockets/controllers/AuthController.java
@@ -0,0 +1,100 @@
+package com.kaleidoscope314.boosterrockets.controllers;
+
+import com.kaleidoscope314.boosterrockets.models.Role;
+import com.kaleidoscope314.boosterrockets.models.User;
+import com.kaleidoscope314.boosterrockets.models.data.RoleRepository;
+import com.kaleidoscope314.boosterrockets.models.data.UserRepository;
+import com.kaleidoscope314.boosterrockets.payload.request.LoginRequest;
+import com.kaleidoscope314.boosterrockets.payload.request.RegisterRequest;
+import com.kaleidoscope314.boosterrockets.payload.response.JwtResponse;
+import com.kaleidoscope314.boosterrockets.payload.response.MessageResponse;
+import com.kaleidoscope314.boosterrockets.security.services.UserAuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.HashSet;
+import java.util.Set;
+
+import static com.kaleidoscope314.boosterrockets.models.ERole.ROLE_ADMIN;
+import static com.kaleidoscope314.boosterrockets.models.ERole.ROLE_USER;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    @Autowired
+    PasswordEncoder encoder;
+
+    @Autowired
+    UserAuthService userAuthService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody RegisterRequest signupRequest) {
+
+        // check and see if user exists
+        if (userRepository.findByName(signupRequest.getEmail()).isPresent()) {
+            return ResponseEntity.badRequest()
+                    .body(new MessageResponse("Error: An account with that email already exists"));
+        }
+
+        User user = new User(signupRequest.getFirstName(), signupRequest.getLastName(),
+                signupRequest.getEmail(), encoder.encode(signupRequest.getPassword()));
+
+        Set<Role> roles = new HashSet<>();
+
+        // workaround to ensure ROLE_USER exists in database
+        if (roleRepository.findByName(ROLE_USER).isEmpty()) {
+            Role userRole = new Role();
+            userRole.setName(ROLE_USER);
+            roleRepository.save(userRole);
+            Role adminRole = new Role();
+            adminRole.setName(ROLE_ADMIN);
+            roleRepository.save(adminRole);
+        }
+
+        // Here we would check what role the newly registered user has and assign it accordingly. For right now, just
+        // gives them the role user
+        Role userRole = roleRepository.findByName(ROLE_USER).get();
+        roles.add(userRole);
+
+        user.setRoles(roles);
+        userRepository.save(user);
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(signupRequest.getEmail(), signupRequest.getPassword()));
+
+        JwtResponse response = userAuthService.generateJwtResponse(authentication);
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> loginUser(@Valid @RequestBody LoginRequest loginRequest) {
+
+        // generate an Authentication object to create token.
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword()));
+
+        JwtResponse response = userAuthService.generateJwtResponse(authentication);
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+}
---
+++ back-end/src/main/java/com/kaleidoscope314/boosterrockets/controllers/UserController.java
@@ -0,0 +1,74 @@
+package com.kaleidoscope314.boosterrockets.controllers;
+
+import com.kaleidoscope314.boosterrockets.models.User;
+import com.kaleidoscope314.boosterrockets.models.data.UserRepository;
+import com.kaleidoscope314.boosterrockets.security.services.UserAuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    UserAuthService userAuthService;
+
+    @Autowired
+    PasswordEncoder encoder;
+
+    @GetMapping("/{userId}")
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<?> getUserById(@PathVariable("userId") int id, @RequestHeader HttpHeaders headers) {
+
+        String headerAuth = headers.getFirst("Authorization");
+
+        if (userRepository.findById(id).isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else if (userAuthService.doesUserMatch(id, headerAuth)) {
+            return new ResponseEntity<>(userRepository.findById(id).get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+    }
+
+    @PutMapping("/{userId}")
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<?> updateUser(@RequestBody User user, @PathVariable("userId") int id,
+                                        @RequestHeader HttpHeaders headers) {
+
+        String headerAuth = headers.getFirst("Authorization");
+
+        if (userRepository.findById(id).isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else if (userAuthService.doesUserMatch(id, headerAuth)) {
+
+            if (user.getPassword() != null) {
+                user.setPassword(encoder.encode(user.getPassword()));
+            } else {
+                user.setPassword(userRepository.findById(id).get().getPassword());
+            }
+
+//            Optional<Iterable<Project>> projectsListOpt = projectRepository.findByUserId(id);
+//            projectsListOpt.ifPresent(projects -> user.setProjects((List<Project>) projects));
+
+            user.setRoles(userRepository.findById(id).get().getRoles());
+
+            userRepository.save(user);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+    }
+}
