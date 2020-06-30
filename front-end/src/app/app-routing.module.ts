e3fdbee849b6474eca60d83c0ced0b985f1e7fee
Carolista <cjones.renaissancewoman@gmail.com>
2020-06-30 17:13:12 -0500

    created admin directory and components for dashboard, create, search, and navbar; put links in navbar

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ import { DeckComponent } from './deck/deck.component';
 import { OptionsComponent } from './options/options.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { NavbarComponent } from './admin/navbar/navbar.component';
+import { CreateComponent } from './admin/create/create.component';
+import { SearchComponent } from './admin/search/search.component';
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
@@ -12,6 +16,10 @@ const routes: Routes = [
   {path: 'start', component: StartComponent},
   {path: 'options', component: OptionsComponent},
   {path: 'deck', component: DeckComponent},
+  {path: 'admin/navbar', component: NavbarComponent},
+  {path: 'admin/dashboard', component: DashboardComponent},
+  {path: 'admin/create', component: CreateComponent},
+  {path: 'admin/search', component: SearchComponent},
   {path: '', redirectTo: 'start', pathMatch: 'full'},
   {path: '**', redirectTo: 'start', pathMatch: 'full'}
 ];
