a962ebecac76a94b3e2f5db093b4e1ad429ec229
Carolista <cjones.renaissancewoman@gmail.com>
2020-07-08 00:24:55 -0500

    refactored create page to pull flashcards fromm database to populate category, topic, and type dropdowns

--- front-end/src/app/admin/create/create.component.ts
+++ front-end/src/app/admin/create/create.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-// import allFlashcards from '../../../assets/question-bank.json';
 import { Flashcard } from 'src/app/flashcard';
+import { TokenStorageService } from 'src/app/_services/token-storage.service';
 
 @Component({
   selector: 'app-create',
@@ -9,18 +9,45 @@ import { Flashcard } from 'src/app/flashcard';
 })
 export class CreateComponent implements OnInit {
 
-  allFlashcards: Flashcard[];
+  dataIsLoaded: boolean = false;
+  flashcardsURL: string = "http://localhost:8080/api/flashcards"
+  allFlashcards: Flashcard[] = [];
+
   allCategories: string[] = [];
   allTopics: string[] = [];
   allTypes: string[] = [];
   
-  // flashcard: Flashcard = new Flashcard("","","","",[],"");
+  flashcard: Flashcard = new Flashcard("","","","","","","","","");
 
-  constructor() { }
+  constructor(private tokenStorageService: TokenStorageService) { }
 
   ngOnInit() {
-    this.buildSelectionArrays();
+    this.loadFlashcards();
   }
+
+  // ** PULL IN ALL FLASHCARDS FROM DATABASE QUESTION BANK ** //
+
+  loadFlashcards() {
+    fetch(this.flashcardsURL, {
+      method: 'GET',
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': 'true',
+        'Authorization': 'Bearer ' + this.tokenStorageService.getToken()
+      }
+    }).then(function(response: any) {
+      response.json().then(function(json) {
+        let questionBank: Flashcard[] = [];
+        json.forEach(obj => {
+          let flashcard = new Flashcard(obj.category, obj.topic, obj.type, obj.query, obj.answer, obj.choiceB, obj.choiceC, obj.choiceD, obj.choiceE);
+          flashcard.id = obj.id;
+          questionBank.push(flashcard);
+        });
+        this.allFlashcards = questionBank;
+        this.buildSelectionArrays();
+      }.bind(this));
+    }.bind(this));
+  }  
 
   findCategory(category: string): number {
     for (let i=0; i < this.allCategories.length; i++) {
@@ -71,11 +98,9 @@ export class CreateComponent implements OnInit {
     this.allCategories.sort((a, b) => (a > b) ? 1 : -1);
     this.allTopics.sort((a, b) => (a > b) ? 1 : -1);
     this.allTypes.sort((a, b) => (a > b) ? 1 : -1);
-  }
 
-  // saveChoiceA() {
-  //   this.flashcard.choices[0] = this.flashcard.answer;
-  // }
+    this.dataIsLoaded = true;
+  }
 
   saveFlashcard() {
     // TODO: write once back end is in place
